852a525e19634b959d94ebeb2cb56a544d7955a2
teammcarthur <chrism.23092@obhs.school.nz>
2025-08-21 09:31:21 +1200

    Lets player win or lose, and part of function comment

--- head_or_tails.py
+++ head_or_tails.py
@@ -9,6 +9,9 @@ import random
 
 
 #-------functions-------------
+#This function gets the user to guess heads or tails until either the player or 
+#computer wins, then 
+
 def heads_tails () :
     user_score=0
     computer_score=0
@@ -17,7 +20,13 @@ def heads_tails () :
         choice=random.randint (0,1)
         computer_guess=options [choice]
         user_guess=str (input ("Heads or Tails") )
-  
+    if user_guess == computer_guess:
+        print ("It was {}, you guessed {}, you won that round".format (computer_guess, user_guess) )
+        user_score +=1
+    else:
+        print ("It was {}, you guessed {}, you lost that round".format (computer_guess, user_guess) )
+        computer_score +=1
+
 #-----main----------------
 if __name__ == '__main__':
     pass
